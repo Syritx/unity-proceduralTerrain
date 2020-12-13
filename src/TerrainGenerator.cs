ab2239765d7fc0629b065bddb7fa339de7deb399
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-12-12 20:11:48 -0500

    added source

---
+++ src/TerrainGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour {
+
+    // main terrain
+    [Range(2, 256)]   public int resolution = 10;
+    [Range(0, 1500)]  public float amplitude = 2;
+    [Range(0,400)]    public float frequency = 1;
+    [Range(.5f, 100)] public float tileScale = 1;
+
+    public float xOffset, yOffset, seed;
+
+    // terrain style
+    [Range(1, 55)]   public int octaves = 1;
+    [Range(0, 1.75f)] public float lacunarity;
+    [Range(0, .5f)]  public float persistance;
+
+    MeshFilter meshFilter;
+    TerrainTile tile;
+    GameObject obj;
+
+    ImprovedNoise noise = new ImprovedNoise();
+
+    private void OnValidate() {
+        Init();
+        GenerateMesh();
+    }
+
+    void Init() {
+
+        if (meshFilter == null) {
+            obj = new GameObject("terrain");
+            obj.transform.parent = transform;
+
+            obj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+            meshFilter = obj.AddComponent<MeshFilter>();
+        }
+
+        obj.transform.localScale = new Vector3(resolution*tileScale, 1, resolution*tileScale);
+
+        tile = new TerrainTile(resolution, octaves, amplitude,
+                               frequency, persistance, lacunarity, xOffset, yOffset, seed, noise);
+        meshFilter.sharedMesh = tile.mesh;
+    }
+
+    void GenerateMesh()
+    {
+        tile.GenerateTerrain();
+    }
+}
---
+++ src/TerrainTIle.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainTile {
+
+    [HideInInspector]
+    public Mesh mesh = new Mesh();
+    int resolution = 0;
+
+    float ampl, freq, lac, per, xOff, yOff, seed;
+    int octaves;
+
+    ImprovedNoise imprNoise;
+
+    public TerrainTile(int resolution, int octaves, float amplitude, float frequency, float persistance, float lacunarity,
+                       float xOff, float yOff, float seed, ImprovedNoise noise) {
+        this.resolution = resolution;
+        ampl = amplitude;
+        freq = frequency;
+
+        this.octaves = octaves;
+        lac = lacunarity;
+        per = persistance;
+
+        this.xOff = xOff;
+        this.yOff = yOff;
+        this.seed = seed;
+
+        this.imprNoise = noise;
+    }
+
+    public void GenerateTerrain()
+    {
+        Vector3[] vertices = new Vector3[(int)Mathf.Pow(resolution,2)];
+        int[] indices = new int[(int)Mathf.Pow(resolution - 1, 2) * 6];
+
+        int idTriangle = 0;
+
+        for (int z = 0; z < resolution; z++) {
+            for (int x = 0; x < resolution; x++) {
+
+                int index = x + z * resolution;
+
+                float heightMap = 0;
+                float frequency = freq;
+                float amplitude = ampl;
+
+                for (int o = 0; o < octaves; o++)
+                {
+                    heightMap += (float)imprNoise.noise((float)(x+xOff)/resolution*frequency, (float)(z + yOff)/resolution*frequency, seed) * amplitude;
+                    frequency *= lac;
+                    amplitude *= per;
+                }
+
+                //MonoBehaviour.print(heightMap);
+
+                Vector2 percent = new Vector2(x, z) / (int)Mathf.Sqrt((indices.Length/6));
+                vertices[index] = Vector3.up + (percent.x - .5f) * 2 * new Vector3(1, 0, 0) + (percent.y - .5f) * 2 * Vector3.Cross(Vector3.up, new Vector3(1,0,0)) + new Vector3(0, heightMap*10,0);
+
+                if (x != resolution-1 && z != resolution-1)
+                {
+                    indices[idTriangle] = index;
+                    indices[idTriangle+1] = index+resolution+1;
+                    indices[idTriangle+2] = index+resolution;
+
+                    indices[idTriangle+3] = index;
+                    indices[idTriangle+4] = index + 1;
+                    indices[idTriangle+5] = index + resolution + 1;
+
+                    idTriangle += 6;
+                }
+            }
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = indices;
+        mesh.RecalculateNormals();
+    }
+}
